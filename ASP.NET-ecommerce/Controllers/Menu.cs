d761aa92514a9cd892b6acb589312e0884af5575
crypto0627 <jake0627a1@gmail.com>
2024-05-15 00:44:41 +0800

    feat: CRUD data

--- ASP.NET-ecommerce/Controllers/Menu.cs
+++ ASP.NET-ecommerce/Controllers/Menu.cs
@@ -1,40 +1,90 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Menu.Data;
-using Menu.Models;
 using Microsoft.EntityFrameworkCore;
+using Menu.Models;
+using System.Threading.Tasks;
 
 namespace Menu.Controllers
 {
-    public class Menu : Controller
+    public class MenuController : Controller
     {
         private readonly MenuContext _context;
-        public Menu(MenuContext context)
+
+        public MenuController(MenuContext context)
         {
             _context = context;
         }
+
         public async Task<IActionResult> Index(string searchString)
         {
             var dishes = from d in _context.Dishes
-                       select d;
-            if(!string.IsNullOrEmpty(searchString))
+                         select d;
+            if (!string.IsNullOrEmpty(searchString))
             {
                 dishes = dishes.Where(d => d.Name.Contains(searchString));
-                return View(await dishes.ToListAsync());
             }
-            return View( await _context.Dishes.ToListAsync() );
+            return View(await dishes.ToListAsync());
         }
+
         public async Task<IActionResult> Details(int? id)
         {
-            var dish = await _context.Dishes
-                .Include( di => di.DishIngredients )
-                .ThenInclude(i => i.Ingredient)
-                .FirstOrDefaultAsync(x => x.Id == id);
-
-            if(dish == null ) 
+            if (id == null)
             {
                 return NotFound();
             }
+
+            var dish = await _context.Dishes
+                .Include(di => di.DishIngredients)
+                .ThenInclude(i => i.Ingredient)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
             return View(dish);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ImageUrl,Price")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dish);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dish);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return BadRequest("Name is required.");
+            }
+
+            var dish = await _context.Dishes.FirstOrDefaultAsync(d => d.Name == name);
+            if (dish == null)
+            {
+                return NotFound($"Dish with name '{name}' not found.");
+            }
+
+            _context.Dishes.Remove(dish);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult SearchDishes(string term)
+        {
+            var dishes = _context.Dishes.Where(d => d.Name.Contains(term)).Select(d => d.Name).ToList();
+            return Json(dishes);
         }
     }
 }
---
+++ ASP.NET-ecommerce/wwwroot/js/Home.js
@@ -0,0 +1,30 @@
+﻿// wwwroot/js/autocomplete.js
+$(document).ready(function () {
+    // 當輸入框的內容發生改變時觸發
+    $('#dishNameInput').on('input', function () {
+        // 獲取輸入的內容
+        var input = $(this).val();
+
+        // 發送 AJAX 請求
+        $.ajax({
+            type: 'GET',
+            url: '/Menu/SearchDishes',
+            data: { term: input },
+            success: function (data) {
+                // 清空建議列表
+                $('#suggestions').empty();
+
+                // 將 AJAX 返回的菜品名稱添加到建議列表中
+                data.forEach(function (item) {
+                    $('#suggestions').append('<div>' + item + '</div>');
+                });
+
+                // 當用戶點擊建議列表中的項目時，將該項目填充到輸入框中
+                $('#suggestions div').on('click', function () {
+                    $('#dishNameInput').val($(this).text());
+                    $('#suggestions').empty();
+                });
+            }
+        });
+    });
+});
